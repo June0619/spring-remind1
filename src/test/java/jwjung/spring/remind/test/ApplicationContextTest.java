0569ce1d9ff774fd3e479ff54a5944d644b0bda1
June0619 <jjw5050@hotmail.com>
2022-05-02 14:52:34 +0900

    전체 빈 조회 테스트코드 추가

--- src/test/java/jwjung/spring/remind/test/ApplicationContextTest.java
+++ src/test/java/jwjung/spring/remind/test/ApplicationContextTest.java
@@ -9,11 +9,28 @@ import jwjung.spring.remind.service.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ApplicationContextTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(MemberConfig.class);
+
+    @Test
+    @DisplayName("전체 애플리케이션 빈 조회하기")
+    void findAllBeans() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanName : beanDefinitionNames) {
+
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+
+                Object bean = ac.getBean(beanName);
+                System.out.println("beanName = " + beanName + " | bean = " + bean);
+            }
+        }
+    }
 
     @Test
     @DisplayName("회원 서비스 빈 찾아오기")
