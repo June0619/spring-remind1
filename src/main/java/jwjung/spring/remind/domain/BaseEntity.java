33e167213b74c79a1589379cd8bd74e42bd79213
June0619 <jjw5050@hotmail.com>
2022-05-14 22:56:54 +0900

    BaseEntity MappedSuperclass 지정

--- src/main/java/jwjung/spring/remind/domain/BaseEntity.java
+++ src/main/java/jwjung/spring/remind/domain/BaseEntity.java
@@ -1,11 +1,22 @@
 package jwjung.spring.remind.domain;
 
-import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToOne;
 import java.time.LocalDateTime;
 
+@MappedSuperclass
+@Getter @Setter
 public abstract class BaseEntity {
 
+    @OneToOne
+    @JoinColumn(name = "MEMBER_ID")
     private Member createdBy;
+    @OneToOne
+    @JoinColumn(name = "MEMBER_ID")
     private Member modifiedBy;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
